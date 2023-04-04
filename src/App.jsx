0b909ee09d13f62450dd96d69f1c4edaa26b0ce6
Josenaldo de Oliveira Matos Filho <josenaldo@gmail.com>
2023-04-03 21:26:26 -0300

    :sparkles: feat: Assets import and template code cleaning

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,15 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
 
-import Counter from '@/components/Counter'
-import Togglable from '@/components/Togglable'
-import Alert from '@/components/Alert'
-import { ALERT_TYPE } from '@/components/Alert'
+import Attribution from '@/components/Attribution'
 
 import './App.css'
 
 function App() {
-  const counter = useSelector((state) => state)
-
-  const [message, setMessage] = React.useState(null)
-
-  const showMessage = () => {
-    let type = null
-
-    if (counter === 0) {
-      type = ALERT_TYPE.INFO
-    }
-
-    if (counter > 0) {
-      type = ALERT_TYPE.SUCCESS
-    }
-
-    if (counter < 0) {
-      type = ALERT_TYPE.ERROR
-    }
-
-    setMessage({
-      type,
-      content: `Counter is now ${counter}`,
-    })
-  }
-
-  const hideMessage = () => {
-    setMessage(null)
-  }
-
-  React.useEffect(() => {
-    showMessage()
-  }, [counter])
-
   return (
     <div className="container">
       <main>
-        <Alert message={message} setMessage={hideMessage} />
-
         <h1>Counter</h1>
-
-        <Togglable buttonLabel="Open">
-          <Counter />
-        </Togglable>
+        <Attribution />
       </main>
     </div>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-import { Provider } from 'react-redux'
-import { createStore } from 'redux'
-
-import counterReducer from '@/reducers/counterReducer'
 import App from './App'
-
-const store = createStore(counterReducer)
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>
 )
